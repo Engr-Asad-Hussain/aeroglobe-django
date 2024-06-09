7ce8ea8ab83e0e7741e0b1d0c59149b01e797473
4snatchgromit <asad@rtss.com.pk>
2024-06-09 08:41:36 +0500

    feat: added pagination in GET /events with attendees as user object

--- django_ems/events/serializers.py
+++ django_ems/events/serializers.py
@@ -1,7 +1,27 @@
-from rest_framework import serializers
+from rest_framework import pagination, serializers
+from rest_framework.response import Response
+from rest_framework.utils.serializer_helpers import ReturnList
 
 from django_ems.events.models import Events
 from django_ems.users_auth.serializers import UsersSerializer
+
+
+class EventsPagination(pagination.PageNumberPagination):
+    page_size = 3
+    page_query_param = "page"
+    max_page_size = 10
+
+    def get_paginated_response(self, data: ReturnList) -> Response:
+        return Response(
+            {
+                "page": self.page.number,
+                "page_size": self.page_size,
+                "count": self.page.paginator.count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
 
 
 class EventsSerializer(serializers.ModelSerializer):
